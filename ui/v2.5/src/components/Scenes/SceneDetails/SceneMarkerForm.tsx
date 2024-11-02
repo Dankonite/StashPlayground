25584a4ece986474b8b957c2d395ec5ec72392bc
MinasukiHikimuna <121475844+MinasukiHikimuna@users.noreply.github.com>
2024-11-02 02:55:48 +0200

    More additions

--- ui/v2.5/src/components/Scenes/SceneDetails/SceneMarkerForm.tsx
+++ ui/v2.5/src/components/Scenes/SceneDetails/SceneMarkerForm.tsx
@@ -232,7 +232,10 @@ export const SceneMarkerForm: React.FC<ISceneMarkerForm> = ({
           value={formik.values.end_seconds}
           setValue={(v) => formik.setFieldValue("end_seconds", v ?? null)}
           onReset={() =>
-            formik.setFieldValue("end_seconds", getPlayerPosition() ?? 0)
+            formik.setFieldValue(
+              "end_seconds",
+              Math.round(getPlayerPosition() ?? 0)
+            )
           }
           error={error}
         />
