8ac2433a35fc64f4339e086b7aa15450116ef429
Draakvoid <Draakvoid@gmail.com>
2024-11-19 23:43:20 -0500

    Added Marker Preview download button

--- ui/v2.5/src/components/Scenes/SceneDetails/Scene.tsx
+++ ui/v2.5/src/components/Scenes/SceneDetails/Scene.tsx
@@ -1522,49 +1522,66 @@ const SceneLoader: React.FC<RouteComponentProps<ISceneParams>> = ({
       )
     );
   }
+
+  const handleDownload = (url: string) => {
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    link.href = url;
+    link.setAttribute('download', '');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
   const markerCar = 
   <div className="markerCar">
-    {scene.scene_markers.map((marker) => <>
-      <div>
-            <Link
-            to={`/scenes/${marker.scene.id}?t=${marker.seconds}`}
-            onClick={() => setPlay(!play)}
+    {scene.scene_markers.map((marker) => (
+      <div key={marker.id} className="marker-preview-container">
+        <Link
+          to={`/scenes/${marker.scene.id}?t=${marker.seconds}`}
+          onClick={() => setPlay(!play)}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "fit-content",
+            padding: "0 .75rem",
+            paddingBottom: "0.25rem",
+            textDecoration: "none",
+            color: "#fff"
+          }}
+        >
+          <img
             style={{
-                display: "flex",
-                flexDirection: "column",
-                width: "fit-content",
-                padding: "0 .75rem",
-                paddingBottom: "0.25rem",
-                textDecoration: "none",
-                color: "#fff"
+              height: "100px",
+              aspectRatio: "auto",
+              borderRadius: ".75rem",
             }}
-            >
-            <img
-                style={{
-                    height: "100px",
-                    aspectRatio: "auto",
-                    borderRadius: ".75rem",
-                }}
-                src={marker.preview}
-            >
-            </img>
-            <span
-                style={{
-                    textAlign: "center"
-                }}
-            >
-                {marker.title ? marker.title : marker.primary_tag.name}
-            </span>
-            <span
-                style={{
-                textAlign: "center"
-            }}
-            >
-                {TextUtils.secondsToTimestamp(marker.seconds)}
-            </span>
-            </Link>
-        </div>
-    </>)}
+            src={marker.preview}
+            alt=""
+          />
+          <span style={{ textAlign: "center" }}>
+            {marker.title ? marker.title : marker.primary_tag.name}
+          </span>
+          <span style={{ textAlign: "center" }}>
+            {TextUtils.secondsToTimestamp(marker.seconds)}
+          </span>
+        </Link>
+        <button
+          className="download-button"
+          onClick={() => handleDownload(marker.stream)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            width="16"
+            height="16"
+          >
+            <path d="M12 16c.55 0 1-.45 1-1V5c0-.55-.45-1-1-1s-1 .45-1 1v10c0 .55.45 1 1 1zm4.29-2.29c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L13 14.17V9c0-.55-.45-1-1-1s-1 .45-1 1v5.17l-1.88-1.88c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l3.29 3.29c.19.19.44.29.71.29s.51-.1.71-.29l3.29-3.29zM18 18H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/>
+          </svg>
+        </button>
+      </div>
+    ))}
   </div>
   const leftDeets = 
   <div className={`floatingdeets ${editMode ? 'edit-mode' : 'view-mode'}`}>
